b2d132c60e6824f87179cf789dcd7a58c2b34d5c
Ankit Rai <raia85859@gmail.com>
2024-06-17 14:38:03 +0545

    Deployemnt Process

--- blog/settings.py
+++ blog/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[BASE_DIR/'static']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
